889da0405661cb4af5a536e10b302f0908b4ce2e
Akshay Dhivare <hi2akshu20497@gmail.com>
2023-10-17 22:21:53 +0530

    second

--- src/components/inner_pages/resturant/Resturant_data.js
+++
@@ -1,7 +0,0 @@
-const resturant_fetch_url =
-  "https://www.swiggy.com/dapi/restaurants/list/v5?lat=19.1998211&lng=72.84259399999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
-
-const Resturant_image =
-  "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
-
-export default resturant_fetch_url;
